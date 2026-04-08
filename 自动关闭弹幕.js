cf6b4ac7400ab2083e4131b97069bae346d3a6ec
TaruaD <1799897046@qq.com>
2026-04-08 21:32:35 +0800

    优化了一下
    优化了一下

--- 自动关闭弹幕.js
+++ 自动关闭弹幕.js
@@ -12,16 +12,22 @@
 (function () {
     'use strict';
     setTimeout(() => {
+        const statusBar = document.querySelector(".bpx-player-dm-root")
+        statusBar.remove()
         const closedanmu = () => {
             const danmu = document.querySelector(".bui-danmaku-switch-input")
             const danmuON = document.querySelector(".bui-danmaku-switch-on")
-            //const danmuOFF = document.querySelector(".bui-danmaku-switch-off")
+            const danmuOFF = document.querySelector(".bui-danmaku-switch-off")
             if (danmuON) {
                 danmu.click()
                 console.log("关闭弹幕")
                 clearInterval(sleepdanmu)
             }
+            if (danmuON) {
+                console.log("弹幕已关闭")
+            }
         }
+
         const sleepdanmu = setInterval(closedanmu, 600)
         closedanmu()
     }, 300)
