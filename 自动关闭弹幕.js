73d108a5da0d390132a7f2af9a4929514e4d60dd
TaruaD <1799897046@qq.com>
2026-04-08 22:26:30 +0800

    优化

--- 自动关闭弹幕.js
+++ 自动关闭弹幕.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      2026-04-06
+// @version      2026-04-08
 // @description  try to take over the world!
 // @author       You
 // @match        *://www.bilibili.com/video/*
@@ -12,23 +12,23 @@
 (function () {
     'use strict';
     setTimeout(() => {
-        const statusBar = document.querySelector(".bpx-player-dm-root")
-        statusBar.remove()
         const closedanmu = () => {
-            const danmu = document.querySelector(".bui-danmaku-switch-input")
-            const danmuON = document.querySelector(".bui-danmaku-switch-on")
-            const danmuOFF = document.querySelector(".bui-danmaku-switch-off")
-            if (danmuON) {
-                danmu.click()
-                console.log("关闭弹幕")
-                clearInterval(sleepdanmu)
+            const danmu = document.querySelector(".bui-danmaku-switch-input");
+            const danmuON = document.querySelector(".bui-danmaku-switch-on");
+            const danmuOFF = document.querySelector(".bui-danmaku-switch-off");
+            if (danmuON || danmuOFF) {
+                if (danmuON) {
+                    danmu.click();
+                    console.log("检测到弹幕开启，执行关闭");
+                } else {
+                    console.log("弹幕已经是关闭状态");
+                }
+                document.querySelector(".bpx-player-dm-root")?.remove();
+                clearInterval(sleepdanmu);
             }
-            if (danmuON) {
-                console.log("弹幕已关闭")
-            }
-        }
+        };
 
-        const sleepdanmu = setInterval(closedanmu, 600)
-        closedanmu()
-    }, 300)
+        const sleepdanmu = setInterval(closedanmu, 600);
+        closedanmu();
+    }, 300);
 })();
